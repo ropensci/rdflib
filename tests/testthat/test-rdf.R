2f0b3c7e1d85893eb998cf9c6f127f2441b1ba05
Carl Boettiger <cboettig@gmail.com>
2017-08-17 21:10:24 -0700

    testthat unit structure

--- tests/testthat/test-rdf.R
+++ tests/testthat/test-rdf.R
@@ -1,11 +1,14 @@
-
+testthat::context("test-rdf.R")
 
 doc <- system.file("extdata/example.rdf", package="redland")
-rdf <- rdf_parse(doc) 
-
 out <- tempfile()
-rdf_serialize(rdf, out, "nquads")
 
+testthat::test_that("we can parse (in rdfxml) and serialize (in nquads) a simple rdf graph", {
+  rdf <- rdf_parse(doc) 
+  rdf_serialize(rdf, out, "nquads")
+})
+
+testthat::test_that("we can make sparql queries", {
 sparql <-
 'PREFIX dc: <http://purl.org/dc/elements/1.1/>
  SELECT ?a ?c
@@ -13,20 +16,36 @@ sparql <-
 
 rdf <- rdf_parse(doc)
 rdf_query(rdf, sparql)
+})
 
+testthat::test_that("we can initialize add triples to rdf graph", {
+  x <- rdf()
+  x <- rdf_add(x, 
+           subject="http://www.dajobe.org/",
+           predicate="http://purl.org/dc/elements/1.1/language",
+           object="en")
+  testthat::expect_is(x, "rdf")
+})
 
-x <- rdf()
-x <- rdf_add(x, 
-         subject="http://www.dajobe.org/",
-         predicate="http://purl.org/dc/elements/1.1/language",
-         object="en")
+testthat::test_that("we can parse and serialize json-ld", {
+  rdf_serialize(x, out, "jsonld")
+  rdf <- rdf_parse(out, format = "jsonld")
+  testthat::expect_is(rdf, "rdf")
+})
 
+testthat::test_that("print and format work", {
+  txt <- format(rdf, format = "rdfxml")
+  print(rdf)
+  
+  testthat::expect_is(txt, "character")
+})
 
-rdf_serialize(x, out, "jsonld")
-rdf <- rdf_parse(out, format = "jsonld")
+testthat::test_that("we can parse from a text string", {
+  rdf <- rdf_parse(doc)
+  txt <- format(rdf, format = "rdfxml")
+  rdf_parse(txt, format="rdfxml")
+})
 
-txt <- format(rdf)
-print(rdf)
 
 unlink(out)
 
